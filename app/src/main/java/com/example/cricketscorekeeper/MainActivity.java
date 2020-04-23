e940a4daeb18de9ad85c6ceeca75325bf0497641
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-04-23 09:34:30 +0530

    Final

--- app/src/main/java/com/example/cricketscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/cricketscorekeeper/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.cricketscorekeeper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,23 +8,40 @@ import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
     Spinner spinner;
-    Button btnSelect,btn0,btn1,btn2,btn3,btn4,btn5,btn6,btne,btnout,btnReset;
-    TextView tvScoreA,tvBallsA,tvScoreB,tvBallsB;
+    Button btnSelect;
+    Button btn0;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Button btn6;
+    Button btne;
+    Button btnout;
+    Button btnReset;
+
+    TextView tvScoreA;
+    TextView tvBallsA;
+    TextView tvScoreB;
+    TextView tvBallsB;
     ScrollView parentLayout;
-    int position=0;
-    String team="";
-    int scoretemaA=0;
-    int getScoretemaB=0;
-    int outA=0;
-    int outB=0;
-    int ballsA=0;
-    int getBallsB=0;
+
+    int position = 0;
+    String team = "";
+    int scoretemaA = 0;
+    int getScoretemaB = 0;
+    int outA = 0;
+    int outB = 0;
+    int ballsA = 0;
+    int getBallsB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,36 +67,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private void findIds() {
         //spinnner ids
-        spinner=findViewById(R.id.spinner);
+        spinner = findViewById(R.id.spinner);
 
         //buttons id
-        btnSelect=findViewById(R.id.btnSelect);
-        btn0=findViewById(R.id.btn0);
-        btn1=findViewById(R.id.btn1);
-        btn2=findViewById(R.id.btn2);
-        btn3=findViewById(R.id.btn3);
-        btn4=findViewById(R.id.btn4);
-        btn5=findViewById(R.id.btn5);
-        btn6=findViewById(R.id.btn6);
-        btne=findViewById(R.id.btne);
-        btnout=findViewById(R.id.btnout);
-        btnReset=findViewById(R.id.btnReset);
+        btnSelect = findViewById(R.id.btnSelect);
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btne = findViewById(R.id.btne);
+        btnout = findViewById(R.id.btnout);
+        btnReset = findViewById(R.id.btnReset);
 
         //textview ids
-        tvScoreA=findViewById(R.id.tvScoreA);
-        tvScoreB=findViewById(R.id.tvScoreB);
-        tvBallsA=findViewById(R.id.tvBallsA);
-        tvBallsB=findViewById(R.id.tvBallsB);
+        tvScoreA = findViewById(R.id.tvScoreA);
+        tvScoreB = findViewById(R.id.tvScoreB);
+        tvBallsA = findViewById(R.id.tvBallsA);
+        tvBallsB = findViewById(R.id.tvBallsB);
 
         //layout
-        parentLayout=findViewById(R.id.parentLayout);
+        parentLayout = findViewById(R.id.parentLayout);
 
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        position=i;
-        team =adapterView.getItemAtPosition(i).toString();
+        position = i;
+        team = adapterView.getItemAtPosition(i).toString();
     }
 
     @Override
@@ -92,8 +106,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onClick(View view) {
-        switch (view.getId())
-        {
+        switch (view.getId()) {
             case R.id.btnSelect:
                 onSelectTeam();
                 break;
@@ -131,68 +144,51 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     private void onReset() {
-        scoretemaA=0;
-        getScoretemaB=0;
-        outA=0;
-        outB=0;
-        ballsA=0;
-        getBallsB=0;
+        scoretemaA = 0;
+        getScoretemaB = 0;
+        outA = 0;
+        outB = 0;
+        ballsA = 0;
+        getBallsB = 0;
         tvBallsA.setText(R.string.nothing);
         tvBallsB.setText(R.string.nothing);
         tvScoreA.setText("No score");
         tvScoreB.setText("No score");
     }
 
-    void onSelectTeam()
-    {
-        if(position==0)
-        {
+    void onSelectTeam() {
+        if (position == 0) {
             Snackbar snackbar = Snackbar.make(parentLayout, "Please Select Team", Snackbar.LENGTH_LONG);
             snackbar.show();
         }
     }
 
-    void setScore(int score)
-    {
-        if(position==1)
-        {
-            if(score>=0)
-            {
-                if(score==8)
-                {
+    void setScore(int score) {
+        if (position == 1) {
+            if (score >= 0) {
+                if (score == 8) {
                     scoretemaA++;
-                }
-                else
-                {
-                    scoretemaA+=score;
+                } else {
+                    scoretemaA += score;
                     ballsA++;
                 }
-            }
-            else
-                {
+            } else {
                 outA++;
             }
-            tvScoreA.setText(scoretemaA+"/"+outA);
+            tvScoreA.setText(scoretemaA + "/" + outA);
             tvBallsA.setText(String.valueOf(ballsA));
-        }
-        else if(position==2)
-        {
-            if(score>=0)
-            {
-                if(score==8)
-                {
+        } else if (position == 2) {
+            if (score >= 0) {
+                if (score == 8) {
                     getScoretemaB++;
-                }
-                else {
+                } else {
                     getScoretemaB += score;
                     getBallsB++;
                 }
-            }
-            else
-            {
+            } else {
                 outB++;
             }
-            tvScoreB.setText(getScoretemaB+"/"+outB);
+            tvScoreB.setText(getScoretemaB + "/" + outB);
             tvBallsB.setText(String.valueOf(getBallsB));
         }
     }
